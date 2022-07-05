3d12c88526580a7725bdd511182c533503851481
Felipe Ortiz <85079457+felipeortizh@users.noreply.github.com>
2022-07-04 20:54:29 -0700

    Update tips.py

--- tips.py
+++ tips.py
@@ -4,16 +4,5 @@
 import streamlit as st
 
 
-pio.renderers.default = "browser"
-
 st.title("A Waiter's Tip Prediction")
-st.header("""
-This Web App predicts the tip given by a customer. 
-""")
 
-st.subheader("""
-Machine Learning model = Multilinear Regression. 
-""")
-
-
-total_bill = st.sidebar.slider("Total bill (in dollars)", 1,300)
